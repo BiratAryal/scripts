32adcfb657f7817935b83dfa5c95be91f63ef5a0
BiratAryal <birataryal1998@gmail.com>
2022-04-16 21:15:06 +0545

    Updated on 04/16/2022 21:15:06

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -1,11 +1,22 @@
 ﻿#dummyfile with 1GB (useful)
-fsutil file createnew D:\delete_directory\download-test\dummy.txt 1073741824
+fsutil file createnew C:\dummy.txt 1073741824
 #for calculating bandwidth using netstat
 $bandwidth = netstat -e
 $new = $bandwidth | select -Skip 4 -First 1|ConvertFrom-String|select @{n='P2(B/s)';e={[int]($_.P2/1024)}}
 echo "the download speed is:"$new
 echo $bandwidth
 ipconfig | grep Signal
+# *******************************************************************************************************
+# Port scan for windows start
+# *******************************************************************************************************
+$value1=Read-Host "Enter the starting port";
+$value2=Read-Host "Enter the ending port";
+$hostaddress=Read-Host "Enter the ip address you want to scan"
+$value1..$value2|ForEach-Object{Write-Output "Port $_ scan result";Test-Connection -TcpPort $_ $hostaddress};
+79..80|ForEach-Object{echo "port scan result of port: $_";Test-Connection -TcpPort $_ www.google.com}
+# *******************************************************************************************************
+# Port scan for windows end
+# ******************************************************************************************************* 
 #Param([Parameter(Mandatory=$True,Position=2)][String]$Name,[Parameter(Mandatory=$True,Position=1)][String]$Greeeting)
 #Write-Host $Greeeting $Name
 
