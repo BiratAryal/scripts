e62a4565d76b4dcc60b7b8de7a6e1df33e6fc863
BiratAryal <birataryal1998@gmail.com>
2022-06-19 23:27:03 +0545

    Updated on 06/19/2022 23:27:03

--- shell-script/restart-wildfly.sh
+++ shell-script/restart-wildfly.sh
@@ -3,13 +3,19 @@
 stopwf(){
    systemctl stop wildfly
    # ( tail -f -n0 /opt/wildfly/standalone/log/server.log & ) | grep -m 1 "stopped in"
-   grep -m 1 "stopped in" <(tail -n 0 -f /opt/wildfly/standalone/log/server.log)
+   # tail -n 1 <(less -F /opt/wildfly/standalone/log/server.log |grep "stopped in")
+   # grep -m 1 "stopped in" <(less -G +F /opt/wildfly/standalone/log/server.log)
+   # grep -m 1 "stopped in" <(tac /opt/wildfly/standalone/log/server.log)
+   sh -c 'tail -n +0 -f /opt/wildfly/standalone/log/server.log | { sed "/stopped in/ q" && kill $$ ;}'
    rcs=$?;    
 }
 startwf(){
    systemctl start wildfly
+   # tail -n 1 <(less -n 0 -F /opt/wildfly/standalone/log/server.log |grep "started in")
+   # grep -m 1 "started in" <(tac /opt/wildfly/standalone/log/server.log)
    # ( tail -f -n0 /opt/wildfly/standalone/log/server.log & ) | grep -m 1 "started in"
-   grep -m 1 "stopped in" <(tail -n 0 -f /opt/wildfly/standalone/log/server.log)
+   # grep -m 1 "started in" <(less -G +F /opt/wildfly/standalone/log/server.log)
+   sh -c 'tail -n +0 -f /opt/wildfly/standalone/log/server.log | { sed "/stopped in/ q" && kill $$ ;}'
    rcr=$?;
 }
 stopwf
